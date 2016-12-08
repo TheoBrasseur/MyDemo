4c00a566aca6265af69949090660aa071ea66ea3
Theo Brasseur <theo.brasseur@imgtec.com>
2016-12-08 14:44:32 +0000

    right spv name

--- Vulkan/recompile.bat
+++ Vulkan/recompile.bat
@@ -1,3 +1,3 @@
 @echo off
-call ..\..\..\..\Builds\Spir-V\compile.bat VertShader_vk.vsh GBufferVertexShader.vsh.spv vert
-call ..\..\..\..\Builds\Spir-V\compile.bat FragShader_vk.fsh GBufferFragmentShader.fsh.spv frag
+call ..\..\..\..\Builds\Spir-V\compile.bat VertShader_vk.vsh VertShader_vk.vsh.spv vert
+call ..\..\..\..\Builds\Spir-V\compile.bat FragShader_vk.fsh FragShader_vk.fsh.spv frag
