b8c6c643643a4d8c6ff25bf66bf30ae227399d5a
Theo Brasseur <theo.brasseur@imgtec.com>
2016-12-08 14:43:36 +0000

    vk ext

--- Vulkan/recompile.bat
+++ Vulkan/recompile.bat
@@ -1,3 +1,3 @@
 @echo off
-call ..\..\..\..\Builds\Spir-V\compile.bat VertShader.vsh GBufferVertexShader.vsh.spv vert
-call ..\..\..\..\Builds\Spir-V\compile.bat FragShader.fsh GBufferFragmentShader.fsh.spv frag
+call ..\..\..\..\Builds\Spir-V\compile.bat VertShader_vk.vsh GBufferVertexShader.vsh.spv vert
+call ..\..\..\..\Builds\Spir-V\compile.bat FragShader_vk.fsh GBufferFragmentShader.fsh.spv frag
